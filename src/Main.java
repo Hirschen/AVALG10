f4f9427a67d9ce6e0916b6505d9688d29bee7952
martin <martin.nycander@gmail.com>
2010-10-24 03:16:25 +0200

    Implemented naive algo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * // TODO: Main is a ...
@@ -16,16 +17,63 @@ public class Main
 
 	}
 
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
-		// TODO: Take input
+		// Read input
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
 
-		// TODO: Run program
+		double[][] graph = new double[size][2];
+		for (int i = 0; i < size; i++)
+		{
+			graph[i][0] = Double.parseDouble(sc.next());
+			graph[i][1] = Double.parseDouble(sc.next());
+		}
 
-		// TODO: Get output
+		// Run algorithm
+		int[] tour = new int[size];
+		boolean[] used = new boolean[size];
+		used[0] = true;
+		for (int i = 1; i < size; i++)
+		{
+			int best = -1;
+			for (int j = 0; j < size; j++)
+			{
+				if (used[j])
+					continue;
+
+				if (best == -1 || dist(graph, tour[i - 1], j) < dist(graph, tour[i - 1], best))
+				{
+					best = j;
+				}
+			}
+			tour[i] = best;
+			used[best] = true;
+		}
+
+		// Output tour
+		for (int i = 0; i < size; i++)
+		{
+			System.out.println(tour[i]);
+		}
+	}
+
+	/**
+	 * @param graph
+	 * @param i
+	 * @param best
+	 * @return
+	 */
+	private static double dist(double[][] graph, int a, int b)
+	{
+		double xDiff = Math.abs(graph[a][0] - graph[b][0]);
+		double yDiff = Math.abs(graph[a][1] - graph[b][1]);
+
+		return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
 	}
 
 }
